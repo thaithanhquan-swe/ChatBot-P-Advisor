7a6be2fc97e2f8035d588d25172937d5059c8413
Thái Thanh Quân <thaithanhquan11102005@gmail.com>
2026-08-17 12:13:23 +0700

    chore: update icons in data

--- client/src/data/data.js
+++ client/src/data/data.js
@@ -1,41 +1,34 @@
-import {
-  AwardIcon,
-  BuildingIcon,
-  CalendarClockIcon,
-  ClipboardListIcon,
-  GraduationCapIcon,
-  WalletIcon,
-} from '@/assets/icons';
+import { Award, Building2, CalendarClock, ClipboardList, GraduationCap, Wallet } from 'lucide-react';
 
 export const popularQuestions = [
   {
     number: '01',
-    icon: ClipboardListIcon,
+    icon: ClipboardList,
     question: 'Điều kiện xét tuyển vào PTIT là gì?',
   },
   {
     number: '02',
-    icon: WalletIcon,
+    icon: Wallet,
     question: 'Học phí của các ngành như thế nào?',
   },
   {
     number: '03',
-    icon: GraduationCapIcon,
+    icon: GraduationCap,
     question: 'PTIT có những ngành đào tạo nào?',
   },
   {
     number: '04',
-    icon: CalendarClockIcon,
+    icon: CalendarClock,
     question: 'Thời gian xét tuyển và nhập học khi nào?',
   },
   {
     number: '05',
-    icon: AwardIcon,
+    icon: Award,
     question: 'Có những loại học bổng nào cho sinh viên?',
   },
   {
     number: '06',
-    icon: BuildingIcon,
+    icon: Building2,
     question: 'Ký túc xá và hỗ trợ sinh viên ra sao?',
   },
 ];
